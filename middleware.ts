0be685e68e31b63eb2ac91b6013d2c62ff83435c
Souma Suzuki <subayai.kobashiri@gmail.com>
2022-10-31 01:33:57 +0900

    fix

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRequest } from 'next/server'
-import logger from './utils/logger'
-
-export function middleware(req: NextRequest) {
-    logger.info({ type: 'access_log', url: req.url })
-}
